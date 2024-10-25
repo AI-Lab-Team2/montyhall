ceb0cfb9204056a1a48103c05df41d8366c4783d
jsonghcbiz <jsong.hcbiz@gmail.com>
2024-10-25 18:51:53 +0900

    feat: setup_game repeat deleted

--- montyhall.py
+++ montyhall.py
@@ -2,7 +2,6 @@ import random  # Import the random library for shuffling and random choices
 
 # Function to set up doors and choose a door for the prize
 def setup_game():
-    def setup_game():
     doors = ["goat", "goat", "car"]  
     random.shuffle(doors)  
     return doors
